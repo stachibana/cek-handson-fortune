0057e50feafcc04c5e255f9aeeeae8f8dab2d919
TACHIBANA SHO <JP21988@w010D71704054m.local>
2018-07-26 15:54:27 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+[誰でも簡単にできる！ LINE Clovaスキルの作り方 第一回](https://codezine.jp/article/detail/10927)のサンプルです。
---
+++ index.js
@@ -0,0 +1,82 @@
+const clova = require('@line/clova-cek-sdk-nodejs');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// 応答の最後に追加するテンプレート
+const TEMPLATE_INQUIRY = '星座を言うか、使い方もしくは終了、と呼びかけて下さい。';
+
+const clovaSkillHandler = clova.Client
+  .configureSkill()
+  // スキルの起動リクエスト
+  .onLaunchRequest(responseHelper => {
+    responseHelper.setSimpleSpeech({
+      lang: 'ja',
+      type: 'PlainText',
+      value: `「サンプル占い」が起動されました。${TEMPLATE_INQUIRY}`,
+    });
+  })
+  // カスタムインテント or ビルトインインテント
+  .onIntentRequest(responseHelper => {
+    const intent = responseHelper.getIntentName();
+    let speech;
+    switch (intent) {
+      // ユーザーのインプットが挨拶だと判別された場合。第2引数はreprompt(入力が行われなかった場合の聞き返し)をするか否か。省略可。
+      case 'FortuneIntent':
+        speech = {
+          lang: 'ja',
+          type: 'PlainText',
+          value: `大吉やで。${TEMPLATE_INQUIRY}`
+        }
+        responseHelper.setSimpleSpeech(speech)
+        responseHelper.setSimpleSpeech(speech, true)
+        // 下記でも可
+        /*
+        responseHelper.setSimpleSpeech(
+          clova.SpeechBuilder.createSpeechText(`ご挨拶して頂きありがとうございます。${TEMPLATE_INQUIRY}`)
+        );
+        */
+        break;
+      // ビルトインインテント。ユーザーによるインプットが使い方のリクエストと判別された場合
+      case 'Clova.GuideIntent':
+        speech = {
+          lang: 'ja',
+          type: 'PlainText',
+          value: TEMPLATE_INQUIRY
+        }
+        responseHelper.setSimpleSpeech(speech)
+        responseHelper.setSimpleSpeech(speech, true)
+        //});
+        break;
+      // ビルトインインテント。ユーザーによるインプットが肯定/否定/キャンセルのみであった場合
+      case 'Clova.YesIntent':
+      case 'Clova.NoIntent':
+      case 'Clova.CancelIntent':
+        speech = {
+          lang: 'ja',
+          type: 'PlainText',
+          value: `意図しない入力です。${TEMPLATE_INQUIRY}`
+        }
+        responseHelper.setSimpleSpeech(speech)
+        break;
+    }
+  })
+  // スキルの終了リクエスト
+  .onSessionEndedRequest(responseHelper => {
+  })
+  .handle();
+
+const app = new express();
+
+// リクエストの検証を行う場合。環境変数APPLICATION_ID(値はClova Developer Center上で入力したExtension ID)が必須
+const clovaMiddleware = clova.Middleware({
+  applicationId: process.env.APPLICATION_ID
+});
+app.post('/clova', clovaMiddleware, clovaSkillHandler);
+
+// リクエストの検証を行わない
+//app.post('/clova', bodyParser.json(), clovaSkillHandler);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
